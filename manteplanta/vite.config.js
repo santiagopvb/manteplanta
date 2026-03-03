3f6f93b668a6adcf76abe0c8f40f55f76dc15751
santiagopvb <95187531+santiagopvb@users.noreply.github.com>
2026-03-03 22:43:20 +0100

    Update vite.config.js

--- manteplanta/vite.config.js
+++ manteplanta/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Si despliegas en GitHub Pages en una subcarpeta tipo:
-  // https://tuusuario.github.io/manteplanta/
-  // descomenta la línea de abajo y cambia 'manteplanta' por el nombre de tu repo
-  // base: '/manteplanta/',
+  base: '/manteplanta/',
 })
